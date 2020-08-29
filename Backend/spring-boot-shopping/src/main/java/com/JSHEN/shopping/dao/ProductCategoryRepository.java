e435be0cc6e8435fad169bb8355f27abd4d549ef
JSHEN9085 <jshen9085@gamil.com>
2020-08-29 19:09:30 -0400

    added frontend

--- Backend/spring-boot-shopping/src/main/java/com/JSHEN/shopping/dao/ProductCategoryRepository.java
+++ Backend/spring-boot-shopping/src/main/java/com/JSHEN/shopping/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.JSHEN.shopping.dao;
 import com.JSHEN.shopping.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") //to prevent cross origin issue: has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 //productCategory is the name of JSON, product-category is the path
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- Backend/spring-boot-shopping/src/main/java/com/JSHEN/shopping/dao/ProductRepository.java
+++ Backend/spring-boot-shopping/src/main/java/com/JSHEN/shopping/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.JSHEN.shopping.dao;
 
 import com.JSHEN.shopping.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("http://localhost:4200") //to prevent cross origin issue: has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource
+//if just copy and paste from browser, it will have 'http://localhost:4200/', but we got to remove the '/' at the very end
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
