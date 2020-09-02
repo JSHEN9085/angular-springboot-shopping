b678788452fbc0017029007b4a116e90084217f7
JSHEN9085 <jshen9085@gamil.com>
2020-09-02 11:32:14 -0400

    added checkout component

--- Frontend/angular-shopping/src/app/app.module.ts
+++ Frontend/angular-shopping/src/app/app.module.ts
@@ -11,10 +11,13 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component'; 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartDetailsComponent } from './components/cart-details/cart-details.component'; 
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 //define Routes, order of routes do matter, start from specific to generic
 const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent}, //component name used what is declared in line 5
   {path: 'category', component: ProductListComponent},
@@ -33,13 +36,15 @@ const routes: Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     RouterModule.forRoot(routes), //routes defined at line 11
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
